65f86082efca28df3d1472d2e059f7c31c19d9d5
ittisafur <ittisafur@gmail.com>
2022-04-12 02:56:55 +0600

    Scaffolding

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,16 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/views/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      colors:{
-        'primary': '#1A1A1A',
-        'secondary': '#FDF1E4',
-      }
+      colors: {
+        primary: "#1A1A1A",
+        secondary: "#FDF1E4",
+      },
     },
   },
   plugins: [],
-}
+};
