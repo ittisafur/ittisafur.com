df5039b2bf02f88ff8a67292c16e17fa2e65f3b8
ittisafur <ittisafur@gmail.com>
2022-04-13 09:46:27 +0600

    Updates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,15 @@
-import type { NextPage } from "next";
+import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return (
-    <div className="px-3 lg:px-0 container mx-auto">
-      <div className="h-screen text-secondary flex justify-center items-center ">
-        <div className="text-center">
-          <h1 className="text-4xl">Staging Version</h1>
+    return (
+        <div className="px-3 lg:px-0 container mx-auto">
+            <div className="h-screen text-secondary flex justify-center items-center ">
+                <div className="text-center">
+                    <h1 className="text-4xl">Staging Version</h1>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Home;
