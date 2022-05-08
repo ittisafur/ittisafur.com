9eb23af488b30880e4412db12cb91c70b6aa639e
ittisafur <ittisafur@gmail.com>
2022-05-08 20:04:52 +0600

    Yarn Workspace | Eslint configs | Updated React and Next

--- next.config.js
+++ next.config.js
@@ -3,9 +3,14 @@ const path = require('path');
 const withImages = require('next-images');
 const withPlugins = require('next-compose-plugins');
 
-const moduleExports = withPlugins([withImages], {
-    reactStrictMode: true,
+const moduleExports = withPlugins([withImages, { silent: false }], {
     swcMinify: true,
+    images: {
+        domains: ['res.cloudinary.com'],
+    },
+    experimental: {
+        outputStandalone: true,
+    },
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
     },
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,20 @@
 import Switcher from '@/layouts/Front';
-import styles from '@/styles/index.module.scss';
 import bindClassNames from 'classnames/bind';
 import type { NextPage } from 'next';
 import { isMobile } from 'react-device-detect';
 import { animated, useSpring } from 'react-spring';
 
+import styles from '@/styles/index.module.scss';
+
 const cx = bindClassNames.bind(styles);
 
 const Home: NextPage = () => {
     const springProps = useSpring({
-        to: { opacity: 1, scale: 1 },
-        from: { opacity: 0, scale: 0.8 },
         config: { duration: 1000 },
+        from: { opacity: 0, scale: 0.8 },
+        to: { opacity: 1, scale: 1 },
     });
+
     return (
         <Switcher>
             <animated.div style={springProps} className={cx('wrapper', { isMobile: isMobile })}>
