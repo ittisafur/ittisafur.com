56264c9032ef46a3186bf643950cc401c429693e
ittisafur <ittisafur@gmail.com>
2022-09-14 20:27:29 +0600

    Polish Portfolio Single Page

--- src/pages/portfolio/[slug]/index.tsx
+++ src/pages/portfolio/[slug]/index.tsx
@@ -34,8 +34,8 @@ const Portfolio: FunctionComponent<Props> = ({ content, settings }) => {
                 keywords={content.stack.map((item) => item.title)}
             />
 
-            <section className="relative">
-                <div className={cx('image-container')}>
+            <section className={cx('banner')}>
+                <div className={cx('banner-container')}>
                     <Image
                         layout="fill"
                         src={media.thumbnail}
@@ -44,9 +44,9 @@ const Portfolio: FunctionComponent<Props> = ({ content, settings }) => {
                     />
                 </div>
 
-                <div className="absolute top-0 left-0 z-30 w-full h-full">
-                    <div className="container mx-auto px-2 py-4 w-full flex flex-col justify-center items-stretch h-full">
-                        <div className="flex flex-col justify-center items-center">
+                <div className={cx('content_wrapper')}>
+                    <div className={cx('layout')}>
+                        <div className={cx('title_status')}>
                             <h1 className={cx('title')}>{title}</h1>
                             <div className={cx('status')}>Status - Finished - {endDate}</div>
                         </div>
@@ -55,18 +55,15 @@ const Portfolio: FunctionComponent<Props> = ({ content, settings }) => {
                                 <h2 className={cx('head')}>Stack</h2>
                                 <div className={cx('list')}>
                                     {stack.map((item, index) => (
-                                        <div
-                                            key={index}
-                                            className="flex flex-col justify-center items-center space-y-2"
-                                        >
-                                            <div className="relative overflow-hidden w-9 h-9 mx-auto object-contain">
+                                        <div key={index} className={cx('item')}>
+                                            <div className={cx('skill', 'icon')}>
                                                 <Image
                                                     src={item.icon}
                                                     alt={item.title}
                                                     layout="fill"
                                                     objectFit="contain"
                                                     objectPosition="center"
-                                                    className="w-9 h-9 mx-auto"
+                                                    className={cx('icon')}
                                                 />
                                             </div>
                                             <p className={cx('title')}>{item.title}</p>
@@ -107,14 +104,8 @@ interface GalleryProps {
 const Gallery: FunctionComponent<GalleryProps> = memo(({ Media }) => {
     return (
         <div className={cx('gallery')}>
-            <h1 className="flex px-2 justify-center lg:px-0 items-center text-xl uppercase font-ProximaNovaBold mb-8 w-full text-center md:text-left">
-                Gallery
-            </h1>
-            <LightGallery
-                plugins={[lgZoom]}
-                mode="lg-fade"
-                elementClassNames="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-4"
-            >
+            <h1 className={cx('title')}>Gallery</h1>
+            <LightGallery plugins={[lgZoom]} mode="lg-fade" elementClassNames={cx('light_gallery')}>
                 {Array.from(Array(6)).map((item, index) => (
                     <a
                         key={index}
@@ -123,12 +114,14 @@ const Gallery: FunctionComponent<GalleryProps> = memo(({ Media }) => {
                         data-tweet-text="Shinimamiya, Osaka, Japan"
                         data-src={Media.thumbnail}
                         data-sub-html="<h4>Photo by - <a href='https://unsplash.com/@entrycube' >Diego Guzmán </a></h4> <p> Location - <a href='https://unsplash.com/s/photos/fushimi-inari-taisha-shrine-senbontorii%2C-68%E7%95%AA%E5%9C%B0-fukakusa-yabunouchicho%2C-fushimi-ward%2C-kyoto%2C-japan'>Fushimi Ward, Kyoto, Japan</a></p>"
-                        className="mx-auto relative overflow-hidden w-72 md:w-80 lg:w-80 xl:w-96 2xl:w-[30rem] h-56 2xl:h-64 cursor-pointer "
+                        className={cx('gallery-container')}
                     >
                         <Image
-                            className="object-cover object-center max-w-xs rounded-md backdrop-blur-lg"
+                            className={cx('image')}
                             src={Media.thumbnail}
                             layout="fill"
+                            objectFit="cover"
+                            objectPosition="center"
                         />
                     </a>
                 ))}
