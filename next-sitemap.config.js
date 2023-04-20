1ce028b08e76397db4d4b3f1f26ec8a4a1a53e53
Ittisafur <ittisafur@gmail.com>
2023-04-20 09:25:03 +0600

    Update with new sitemap settings

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,4 +3,7 @@
 module.exports = {
     siteUrl: process.env.SITE_URL || 'https://ittisafur.com',
     generateRobotsTxt: true,
+    robotsTxtOptions: {
+      additionalSitemaps: [ `${process.env.SITE_URL}/portfolio/sitemap.xml` ],
+    }
 };
--- src/pages/portfolio/index.tsx
+++ src/pages/portfolio/index.tsx
@@ -1,9 +1,11 @@
 import PortfolioExcerpt from 'components/PortfolioExcept';
 import React, { Fragment } from 'react';
+import SEO from 'utils/SEO';
 
 const Portfolio = (props: {}) => {
     return (
         <Fragment>
+            <SEO title="Portfolio Page" description="Portfolio Page" image="" keywords="" />
             <PortfolioExcerpt />
         </Fragment>
     );
