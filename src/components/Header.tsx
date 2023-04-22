f4cc6528248ab26c55e8bf4ae6a6cbf504f06a33
Ittisafur <ittisafur@gmail.com>
2023-04-22 13:10:58 +0600

    Update Overlay Menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Fragment } from 'react';
-import { Popover, Transition } from '@headlessui/react';
+import { Fragment, useState } from 'react';
+import { Popover, Transition, Dialog } from '@headlessui/react';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import Image from 'next/image';
@@ -10,77 +10,113 @@ import bindClassNames from 'classnames/bind';
 const cx = bindClassNames.bind(styles);
 
 function Header() {
+    let [isOpen, setIsOpen] = useState(false);
     return (
-        <Popover className={cx('wrapper')}>
-            <div className={cx('menu')}>
-                <div className={cx('left')}>
-                    <Link href="/" passHref>
-                        <span className={cx('screen-reader')}>
-                            Ittisafur - Full Stack Developer
-                        </span>
+        <div className={cx('nav')}>
+            <div className={cx('nav-container')}>
+                <div className={cx('navbar')}>
+                    <div className={cx('logo')}>
                         <Image
                             src="/assets/svg/logo-white.svg"
                             alt="Ittisafur Logo"
-                            width={100}
-                            height={100}
+                            width="100"
+                            height="50"
                         />
-                    </Link>
-                </div>
-
-                <div className="-mr-2 -my-2 md:hidden">
-                    <Popover.Button className="rounded-md p-2 inline-flex items-center justify-center hover:text-secondary hover:text-opacity-70 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-secondary">
-                        <span className={cx('screen-reader')}>Open menu</span>
-                        <Bars3Icon className={cx('icon')} aria-hidden="true" />
-                    </Popover.Button>
-                </div>
-
-                <Popover.Group as="nav" className={cx('right')}>
-                    <Link href="/portfolio" className={cx('link')}>
-                        Portfolio
-                    </Link>
-
-                    <Link href="/" className={cx('link')}>
-                        Contact
-                    </Link>
-                </Popover.Group>
-            </div>
-
-            <Transition
-                as={Fragment}
-                enter="duration-200 ease-out"
-                enterFrom="opacity-0 scale-95"
-                enterTo="opacity-100 scale-100"
-                leave="duration-100 ease-in"
-                leaveFrom="opacity-100 scale-100"
-                leaveTo="opacity-0 scale-95"
-            >
-                <Popover.Panel focus className={cx('mobile')}>
-                    <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-primary divide-y-2 divide-gray-50">
-                        <div className="pt-5 pb-6 px-2">
-                            <div className="flex items-center justify-end">
-                                <div className="-mr-2">
-                                    <Popover.Button className="bg-primary rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-secondary">
-                                        <span className={cx('screen-reacer')}>Close menu</span>
-                                        <XMarkIcon className={cx('icon')} aria-hidden="true" />
-                                    </Popover.Button>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="py-6 px-2 space-y-6">
-                            <div className="grid grid-cols-1 gap-y-4 gap-x-8 justify-items-center">
-                                <Link href="/portfolio" className={cx('link')}>
-                                    Portfolio
-                                </Link>
-
-                                <Link href="/" className={cx('link')}>
-                                    Contact
-                                </Link>
-                            </div>
+                    </div>
+                    <div className={cx('menu-toggle')} onClick={() => setIsOpen(!isOpen)}>
+                        <div
+                            className={cx('hambox', {
+                                'ham-box-open': isOpen,
+                            })}
+                        >
+                            <span
+                                className={cx('line-top', {
+                                    spin: isOpen,
+                                })}
+                            ></span>
+                            <span className={cx('line-bottom', { spin: isOpen })}></span>
                         </div>
                     </div>
-                </Popover.Panel>
-            </Transition>
-        </Popover>
+                </div>
+                <div
+                    className={cx('nav-overlay')}
+                    style={{
+                        top: isOpen ? '0' : '-100%',
+                        transitionDelay: isOpen ? '0.3s' : '0s',
+                    }}
+                >
+                    <ul className={cx('nav-links')}>
+                        <li className={cx('items')}>
+                            <Link
+                                href="/"
+                                className={cx('link')}
+                                onClick={() => setIsOpen(!isOpen)}
+                                style={{
+                                    top: isOpen ? '0' : '7.5rem',
+                                }}
+                            >
+                                Home
+                            </Link>
+                            <div className={cx('item-wrapper')} />
+                        </li>
+
+                        <li className={cx('items')}>
+                            <Link
+                                href="/"
+                                className={cx('link')}
+                                onClick={() => setIsOpen(!isOpen)}
+                                style={{
+                                    top: isOpen ? '0' : '7.5rem',
+                                }}
+                            >
+                                Portfolio
+                            </Link>
+                            <div className={cx('item-wrapper')} />
+                        </li>
+                        {/* <li className={cx('items',)}> */}
+                        {/*     <Link */}
+                        {/*         href="/" */}
+                        {/*         className={cx('link')} */}
+                        {/*         onClick={() => setIsOpen(!isOpen)} */}
+                        {/*         style={{ */}
+                        {/*             top: isOpen ? '0' : '7.5rem', */}
+                        {/*         }} */}
+                        {/*     > */}
+                        {/*         Service */}
+                        {/*     </Link> */}
+                        {/*     <div className={cx('item-wrapper')} /> */}
+                        {/* </li> */}
+
+                        {/* <li className={cx('items')}> */}
+                        {/*     <Link */}
+                        {/*         href="/" */}
+                        {/*         className={cx('link')} */}
+                        {/*         onClick={() => setIsOpen(!isOpen)} */}
+                        {/*         style={{ */}
+                        {/*             top: isOpen ? '0' : '7.5rem', */}
+                        {/*         }} */}
+                        {/*     > */}
+                        {/*         Blog */}
+                        {/*     </Link> */}
+                        {/*     <div className={cx('item-wrapper')} /> */}
+                        {/* </li> */}
+                        {/* <li className={cx('items')}> */}
+                        {/*     <Link */}
+                        {/*         href="/" */}
+                        {/*         className={cx('link')} */}
+                        {/*         onClick={() => setIsOpen(!isOpen)} */}
+                        {/*         style={{ */}
+                        {/*             top: isOpen ? '0' : '7.5rem', */}
+                        {/*         }} */}
+                        {/*     > */}
+                        {/*         Contact */}
+                        {/*     </Link> */}
+                        {/*     <div className={cx('item-wrapper')} /> */}
+                        {/* </li> */}
+                    </ul>
+                </div>
+            </div>
+        </div>
     );
 }
 
