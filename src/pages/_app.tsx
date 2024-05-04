ed6cdc2fb993d12fd57aa6e311591e0b4d6d82ae
Ittisafur <ittisafur@gmail.com>
2024-05-05 05:42:43 +0600

    Add Posthog

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,11 +7,24 @@ import Footer from 'components/Footer';
 import { Analytics } from '@vercel/analytics/react';
 import TagManager from 'react-gtm-module';
 import Script from 'next/script';
+import posthog from 'posthog-js';
+import { PostHogProvider } from 'posthog-js/react';
+
+if (typeof window !== 'undefined') {
+    // checks that we are client-side
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+        api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
+        loaded: (posthog) => {
+            if (process.env.NODE_ENV === 'development') posthog.debug(); // debug mode in development
+        },
+    });
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
     useEffect(() => {
         TagManager.initialize({ gtmId: process.env.NEXT_PUBLIC_GTM_ID });
     }, []);
+    posthog.capture('my event', { property: 'value' });
 
     return (
         <Fragment>
@@ -28,9 +41,11 @@ function MyApp({ Component, pageProps }: AppProps) {
                 gtag('config', 'G-7VYVZFTH80');
               `}
             </Script>
-            <Header />
-            <Component {...pageProps} />
-            <Footer />
+            <PostHogProvider client={posthog}>
+                <Header />
+                <Component {...pageProps} />
+                <Footer />
+            </PostHogProvider>
             <Analytics />
         </Fragment>
     );
