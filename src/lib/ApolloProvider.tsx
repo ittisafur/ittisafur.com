15f26802bd89b115e13c71fe1810b29c7ee28b62
Ittisafur <ittisafur@gmail.com>
2025-02-04 04:03:26 +0600

    Fix Endpoint

--- src/lib/ApolloProvider.tsx
+++ src/lib/ApolloProvider.tsx
@@ -14,7 +14,7 @@ function makeClient() {
 
         uri: process.env.NEXT_PUBLIC_API
             ? process.env.NEXT_PUBLIC_API
-            : 'https://endpoints.ittisafur.com',
+            : 'https://endpoints.ittisafur.com/graphql',
         // you can disable result caching here if you want to
         // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
         fetchOptions: { cache: 'no-store' },
